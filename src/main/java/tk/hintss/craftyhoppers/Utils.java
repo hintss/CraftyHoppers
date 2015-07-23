f1f513f3456a99308184247cd167ca4cf9b969ba
Henry Ammermann <hintsss@gmail.com>
2015-07-23 03:58:06 -0700

    sorting works for picking up items, not quite for movements between hoppers, though

--- src/main/java/tk/hintss/craftyhoppers/Utils.java
+++ src/main/java/tk/hintss/craftyhoppers/Utils.java
@@ -3,7 +3,7 @@ package tk.hintss.craftyhoppers;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
-import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.LinkedList;
 
@@ -13,24 +13,68 @@ import java.util.LinkedList;
 public class Utils {
     private static BlockFace[] SIGN_DIRECTIONS = new BlockFace[] {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST, BlockFace.UP};
 
-    public static Sign getAttachedSign(Block b) {
+    public static LinkedList<Sign> getAttachedSigns(Block b) {
         if (b == null) {
             return null;
         }
 
         LinkedList<Sign> signs = new LinkedList<>();
 
-        if (b.getState() instanceof InventoryHolder) {
-            for (BlockFace bf : SIGN_DIRECTIONS) {
-                Block possibleSign = b.getRelative(bf);
-                if (possibleSign.getState() instanceof Sign && ((Sign) possibleSign.getState()).getLine(0).equalsIgnoreCase("[infinitechest]")) {
-                    if (((org.bukkit.material.Sign) possibleSign.getState().getData()).getAttachedFace() == bf.getOppositeFace()) {
-                        return (Sign) possibleSign.getState();
-                    }
+        for (BlockFace bf : SIGN_DIRECTIONS) {
+            Block possibleSign = b.getRelative(bf);
+            if (possibleSign.getState() instanceof Sign) {
+                if (((org.bukkit.material.Sign) possibleSign.getState().getData()).getAttachedFace() == bf.getOppositeFace()) {
+                    signs.add(((Sign) possibleSign.getState()));
                 }
             }
         }
 
-        return null;
+        return signs;
+    }
+
+    public static boolean itemMatchesList(String s, ItemStack is) {
+        String[] split = s.split(",");
+
+        for (String item : split) {
+            if (item.contains(":")) {
+                String[] bits = item.split(":");
+
+                item = bits[0];
+                String damage = bits[1];
+
+                short isDmg = is.getDurability();
+
+                if (!isInt(damage)) continue;
+                if (Short.valueOf(damage) != isDmg) continue;
+            }
+
+            if (!isInt(item)) {
+                continue;
+            }
+
+            if (Integer.valueOf(item) == is.getType().getId()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isInt(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (c < '0') {
+                return false;
+            }
+
+            if (c > '9') {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/main/java/tk/hintss/craftyhoppers/listeners/HopperListener.java
+++ src/main/java/tk/hintss/craftyhoppers/listeners/HopperListener.java
@@ -1,10 +1,13 @@
 package tk.hintss.craftyhoppers.listeners;
 
 import org.bukkit.block.Hopper;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+import tk.hintss.craftyhoppers.Utils;
 
 /**
  * Created by hintss on 7/22/2015.
@@ -12,15 +15,46 @@ import org.bukkit.event.inventory.InventoryPickupItemEvent;
 public class HopperListener implements Listener {
     @EventHandler
     public void onItemPickup(InventoryPickupItemEvent event) {
+        if (!(event.getInventory().getHolder() instanceof Hopper)) {
+            return;
+        }
 
+        boolean shouldCancel = false;
+
+        shouldCancel = !hopperShouldIngest(((Hopper) event.getInventory().getHolder()), event.getItem().getItemStack());
+
+        if (shouldCancel) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
     public void onItemMove(InventoryMoveItemEvent event) {
+        if (!(event.getDestination() instanceof Hopper)) {
+            return;
+        }
 
+        boolean shouldCancel = false;
+
+        shouldCancel = !hopperShouldIngest((Hopper) event.getDestination().getHolder(), event.getItem());
+
+        if (shouldCancel) {
+            event.setCancelled(true);
+        }
     }
 
-    private void doHopperStuffs(Hopper h) {
+    private boolean hopperShouldIngest(Hopper h, ItemStack is) {
+        for (Sign sign : Utils.getAttachedSigns(h.getBlock())) {
+            // item sorting
+            if (sign.getLine(0).equals("[sort]")) {
+                for (int i = 1; i < 4; i++) {
+                    if (Utils.itemMatchesList(sign.getLine(i), is)) return true;
+                }
 
+                return false;
+            }
+        }
+
+        return true;
     }
 }
