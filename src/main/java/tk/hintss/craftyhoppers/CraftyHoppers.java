bc346c70d4b05411b3530ba579836bc252be646b
Henry Ammermann <hintsss@gmail.com>
2015-07-23 02:39:41 -0700

    probably the bare minimum to start dev-ing with jrebel

--- src/main/java/tk/hintss/craftyhoppers/CraftyHoppers.java
+++ src/main/java/tk/hintss/craftyhoppers/CraftyHoppers.java
@@ -1,7 +1,15 @@
 package tk.hintss.craftyhoppers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import tk.hintss.craftyhoppers.listeners.HopperListener;
+
 /**
  * Created by hintss on 7/22/2015.
  */
-public class CraftyHoppers {
+public class CraftyHoppers extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new HopperListener(), this);
+    }
 }
---
+++ src/main/java/tk/hintss/craftyhoppers/listeners/HopperListener.java
@@ -0,0 +1,26 @@
+package tk.hintss.craftyhoppers.listeners;
+
+import org.bukkit.block.Hopper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
+
+/**
+ * Created by hintss on 7/22/2015.
+ */
+public class HopperListener implements Listener {
+    @EventHandler
+    public void onItemPickup(InventoryPickupItemEvent event) {
+
+    }
+
+    @EventHandler
+    public void onItemMove(InventoryMoveItemEvent event) {
+
+    }
+
+    private void doHopperStuffs(Hopper h) {
+
+    }
+}
