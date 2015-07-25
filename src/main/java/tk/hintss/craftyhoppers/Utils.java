56a34395025c50b1546c5ff2c61837ff551b2f89
Henry Ammermann <hintsss@gmail.com>
2015-07-25 02:12:05 -0700

    start adding recipe stuff

--- src/main/java/tk/hintss/craftyhoppers/Utils.java
+++ src/main/java/tk/hintss/craftyhoppers/Utils.java
@@ -1,17 +1,24 @@
 package tk.hintss.craftyhoppers;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by hintss on 7/22/2015.
  */
 public class Utils {
     private static BlockFace[] SIGN_DIRECTIONS = new BlockFace[] {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST, BlockFace.UP};
+    private static BlockFace[] BLOCK_DIRECTIONS = new BlockFace[] {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST, BlockFace.UP, BlockFace.DOWN};
 
     public static LinkedList<Sign> getAttachedSigns(Block b) {
         if (b == null) {
@@ -32,6 +39,21 @@ public class Utils {
         return signs;
     }
 
+    public static boolean hasAdjacent(Block b, Material mat) {
+        if (b == null) {
+            return false;
+        }
+
+        for (BlockFace bf : BLOCK_DIRECTIONS) {
+            Block possible = b.getRelative(bf);
+            if (possible.getType() == mat) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static boolean itemMatchesList(String s, ItemStack is) {
         String[] split = s.split(",");
 
@@ -44,8 +66,13 @@ public class Utils {
 
                 short isDmg = is.getDurability();
 
-                if (!isInt(damage)) continue;
-                if (Short.valueOf(damage) != isDmg) continue;
+                if (!isInt(damage)) {
+                    continue;
+                }
+                
+                if (Short.valueOf(damage) != isDmg) {
+                    continue;
+                }
             }
 
             if (!isInt(item)) {
@@ -58,6 +85,53 @@ public class Utils {
         }
 
         return false;
+    }
+
+    public static List<Recipe> getRecipes(String s) {
+        LinkedList<Recipe> recipes = new LinkedList<>();
+
+        String[] split = s.split(",");
+
+        for (String item : split) {
+            short dmg = -1;
+
+            if (item.contains(":")) {
+                String[] bits = item.split(":");
+
+                item = bits[0];
+                String damage = bits[1];
+
+                if (!isInt(damage)) {
+                    continue;
+                }
+
+                dmg = Short.valueOf(damage);
+            }
+
+            if (!isInt(item)) {
+                continue;
+            }
+
+            int id = Integer.valueOf(item);
+
+            ItemStack is = new ItemStack(id, 1, dmg);
+
+            for (Recipe r : Bukkit.getRecipesFor(is)) {
+                if (r instanceof ShapedRecipe || r instanceof ShapelessRecipe) {
+                    recipes.add(r);
+                }
+            }
+        }
+
+        return recipes;
+    }
+
+    public List<ItemStack> getRecipeIngredients(Recipe r) {
+        if (r instanceof ShapelessRecipe) {
+
+        } else if (r instanceof ShapedRecipe) {
+
+        }
     }
 
     public static boolean isInt(String s) {
--- src/main/java/tk/hintss/craftyhoppers/listeners/HopperListener.java
+++ src/main/java/tk/hintss/craftyhoppers/listeners/HopperListener.java
@@ -1,13 +1,19 @@
 package tk.hintss.craftyhoppers.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.block.Hopper;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import tk.hintss.craftyhoppers.Utils;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by hintss on 7/22/2015.
@@ -44,6 +50,7 @@ public class HopperListener implements Listener {
     }
 
     private boolean hopperShouldIngest(Hopper h, ItemStack is) {
+        // whether this itemstack has encountered a sign that didn't like it yet
         boolean negativeEvidence = false;
 
         for (Sign sign : Utils.getAttachedSigns(h.getBlock())) {
@@ -54,6 +61,23 @@ public class HopperListener implements Listener {
                 }
 
                 negativeEvidence = true;
+            } else if (sign.getLine(0).equals("[craft]")) {
+                // item crafting
+
+                // require an adjacent workbench
+                if (!Utils.hasAdjacent(h.getBlock(), Material.WORKBENCH)) {
+                    continue;
+                }
+
+                List<Recipe> recipes = new LinkedList<>();
+
+                for (int i = 1; i < 4; i++) {
+                    recipes.addAll(Utils.getRecipes(sign.getLine(i)));
+                }
+
+                Inventory i = h.getInventory();
+
+
             }
         }
 
