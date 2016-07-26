48da9a815a889f1be2a22a885c01a2796c84f036
Henry Ammermann <hintsss@gmail.com>
2016-07-25 22:44:42 -0700

    do recipe listing after startup

--- src/main/java/pw/hintss/craftyhoppers/CraftyHoppers.java
+++ src/main/java/pw/hintss/craftyhoppers/CraftyHoppers.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import pw.hintss.craftyhoppers.listeners.HopperListener;
 
 import java.util.Iterator;
@@ -14,17 +15,24 @@ import java.util.Iterator;
 public class CraftyHoppers extends JavaPlugin {
     @Override
     public void onEnable() {
-        // generate the list of smeltable items
-        for (Iterator<Recipe> iter = Bukkit.recipeIterator(); iter.hasNext(); ) {
-            Recipe r = iter.next();
-
-            if (r instanceof FurnaceRecipe) {
-                FurnaceRecipe smelt = (FurnaceRecipe) r;
-                Utils.SMELTABLE.add(smelt.getInput().getType());
-                getLogger().info("added smelting recipe for " + smelt.getInput().getType().name() + " -> " + smelt.getResult().getType().name());
-            }
-        }
-
         Bukkit.getPluginManager().registerEvents(new HopperListener(), this);
+
+        // start generating lists based on recipes
+        // this is done after startup just in case other plugins add/modify recipes
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // generate the list of smeltable items
+                for (Iterator<Recipe> iter = Bukkit.recipeIterator(); iter.hasNext(); ) {
+                    Recipe r = iter.next();
+
+                    if (r instanceof FurnaceRecipe) {
+                        FurnaceRecipe smelt = (FurnaceRecipe) r;
+                        Utils.SMELTABLE.add(smelt.getInput().getType());
+                        getLogger().info("added smelting recipe for " + smelt.getInput().getType().name() + " -> " + smelt.getResult().getType().name());
+                    }
+                }
+            }
+        }.runTaskLater(this, 1L);
     }
 }
